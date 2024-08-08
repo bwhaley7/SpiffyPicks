3500a031170feb6aa12ab75b4dae7c0ae5d6f1e7
Braden <braden.whaley7@gmail.com>
2024-08-08 10:14:23 -0400

    .env db string is now pulled from init and passed to all scrapers

--- scrapers/picks/__init__.py
+++ scrapers/picks/__init__.py
@@ -1,13 +1,23 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 from covers import scrape_covers
 from dimers import scrape_dimers
 from pickswise import scrape_pickswise
 from wagertalk import scrape_wagertalk
 
+load_dotenv(find_dotenv())
+
+# Access the MongoDB connection string from the environment variable
+mongo_db_string = os.getenv('MONGO_DB_CONNECTION_STRING')
+
+if mongo_db_string is None:
+    print("MongoDB connection string not found. Please check your .env file.")
+
 def run_all_scrapers():
-    scrape_covers()
-    scrape_dimers()
-    scrape_pickswise()
-    scrape_wagertalk()
+    scrape_covers(mongo_db_string)
+    scrape_dimers(mongo_db_string)
+    scrape_pickswise(mongo_db_string)
+    scrape_wagertalk(mongo_db_string)
 
 if __name__ == "__main__":
     run_all_scrapers()
