58c3ebbcb3864659d900e22eb8334efea94f3333
Braden <braden.whaley7@gmail.com>
2024-08-08 11:15:53 -0400

    init file is now in root of scrapers folder and will run all scrape jobs

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from covers import scrape_covers
-from dimers import scrape_dimers
-from pickswise import scrape_pickswise
-from wagertalk import scrape_wagertalk
+from picks.covers import scrape_covers
+from picks.dimers import scrape_dimers
+from picks.pickswise import scrape_pickswise
+from picks.wagertalk import scrape_wagertalk
+from projections.sportsline import scrape_sportsline_dfs
+from pymongo import MongoClient
 
 load_dotenv(find_dotenv())
 
@@ -14,10 +16,20 @@ if mongo_db_string is None:
     print("MongoDB connection string not found. Please check your .env file.")
 
 def run_all_scrapers():
+    #Clear all data debug method for testing
+    client = MongoClient(mongo_db_string)
+    db = client['spiffypicks']
+    collection = db['scraped_picks']
+    collection2 = db['scraped_projections']
+
+    result = collection.delete_many({})
+    result2 = collection2.delete_many({})
+
     scrape_covers(mongo_db_string)
     scrape_dimers(mongo_db_string)
     scrape_pickswise(mongo_db_string)
     scrape_wagertalk(mongo_db_string)
+    scrape_sportsline_dfs(mongo_db_string)
 
 if __name__ == "__main__":
     run_all_scrapers()
