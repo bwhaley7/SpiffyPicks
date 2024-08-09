cfa4d31fff843be2f5e2f40575f4e4bcc28106ee
Braden <braden.whaley7@gmail.com>
2024-08-09 12:00:54 -0400

    added oddsshark scraping

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -4,6 +4,7 @@ from picks.covers import scrape_covers
 from picks.dimers import scrape_dimers
 from picks.pickswise import scrape_pickswise
 from picks.wagertalk import scrape_wagertalk
+from picks.oddsshark import scrape_oddsshark
 from projections.sportsline import scrape_sportsline_dfs
 from projections.fantasypros import scrape_fantasypros
 from pymongo import MongoClient
@@ -26,12 +27,13 @@ def run_all_scrapers():
     result = collection.delete_many({})
     result2 = collection2.delete_many({})
 
-    scrape_covers(mongo_db_string)
-    scrape_dimers(mongo_db_string)
-    scrape_pickswise(mongo_db_string)
-    scrape_wagertalk(mongo_db_string)
-    scrape_sportsline_dfs(mongo_db_string)
-    scrape_fantasypros(mongo_db_string, 1) #number is for the NFL week you are on.
+    #scrape_covers(mongo_db_string)
+    #scrape_dimers(mongo_db_string)
+    #scrape_pickswise(mongo_db_string)
+    #scrape_wagertalk(mongo_db_string)
+    scrape_oddsshark(mongo_db_string)
+    #scrape_sportsline_dfs(mongo_db_string)
+    #scrape_fantasypros(mongo_db_string, 1) #number is for the NFL week you are on.
 
 if __name__ == "__main__":
     run_all_scrapers()
