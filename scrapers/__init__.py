f260e2a84340b240aa7e9ede7c9771f582a1bfef
Braden <braden.whaley7@gmail.com>
2024-08-21 11:39:09 -0400

    general fixes

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,6 +1,8 @@
 import os
+import json
+from json import JSONEncoder
 from dotenv import load_dotenv, find_dotenv
-from picks.covers import scrape_covers
+from picks.covers import scrape_picks_covers
 from picks.dimers import scrape_dimers
 from picks.pickswise import scrape_pickswise
 from picks.wagertalk import scrape_wagertalk
@@ -9,37 +11,57 @@ from projections.sportsline import scrape_sportsline_dfs
 from projections.fantasypros import scrape_fantasypros
 from articles.actionnetwork import scrape_action_articles
 from articles.coversArticles import scrape_covers_articles
-from pymongo import MongoClient
+from odds.bettingpros_game_odds import output_game_odds_file
+from odds.format_odds_output import format_odds
+from datetime import datetime, date
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (date, datetime)):
+            return obj.isoformat()
+        return super().default(obj)
 
 load_dotenv(find_dotenv())
 
-# Access the MongoDB connection string from the environment variable
-mongo_db_string = os.getenv('MONGO_DB_CONNECTION_STRING')
-
-if mongo_db_string is None:
-    print("MongoDB connection string not found. Please check your .env file.")
+dataPath = os.getenv('DATA_PATH')
 
 def run_all_scrapers():
-    #Clear all data debug method for testing
-    client = MongoClient(mongo_db_string)
-    db = client['spiffypicks']
-    collection = db['scraped_picks']
-    collection2 = db['scraped_projections']
-    collection3 = db['game_articles']
+    covers_picks = scrape_picks_covers()
+    dimers_picks = scrape_dimers()
+    pickswise_picks = scrape_pickswise()
+    wagertalk_picks = scrape_wagertalk()
+    #oddsshark_picks = scrape_oddsshark()
+    fp_proj = scrape_fantasypros(2) #number is for the NFL week you are on.
+    action_articles = scrape_action_articles()
+    covers_articles = scrape_covers_articles()
 
-    result = collection.delete_many({})
-    result2 = collection2.delete_many({})
-    result3 = collection3.delete_many({})
+    output_game_odds_file("pre3", dataPath)
+    format_odds("pre3", dataPath)
 
-    scrape_covers(mongo_db_string)
-    scrape_dimers(mongo_db_string)
-    scrape_pickswise(mongo_db_string)
-    scrape_wagertalk(mongo_db_string)
-    scrape_oddsshark(mongo_db_string)
-    scrape_sportsline_dfs(mongo_db_string)
-    scrape_fantasypros(mongo_db_string, 1) #number is for the NFL week you are on.
-    scrape_action_articles(mongo_db_string)
-    scrape_covers_articles(mongo_db_string)
+    expert_picks = (
+    covers_picks +
+    dimers_picks +
+    pickswise_picks +
+    wagertalk_picks #+
+    #oddsshark_picks
+    )
+
+    analyst_articles = (
+        action_articles +
+        covers_articles
+    )
+
+    # Write combined data to JSON file
+    with open(os.path.join(dataPath, 'expert_picks.json'), 'w', encoding='utf-8') as file:
+        json.dump(expert_picks, file, ensure_ascii=False, indent=4, cls=CustomJSONEncoder)
+    
+    with open(os.path.join(dataPath, 'analyst_articles.json'), 'w', encoding='utf-8') as file:
+        json.dump(analyst_articles, file, ensure_ascii=False, indent=4, cls=CustomJSONEncoder)
+
+    with open(os.path.join(dataPath, 'player_projections.json'), 'w', encoding='utf-8') as file:
+        json.dump(fp_proj, file, ensure_ascii=False, indent=4, cls=CustomJSONEncoder)
+
+    print(f"All data has been written to their respective files.")
 
 if __name__ == "__main__":
     run_all_scrapers()
