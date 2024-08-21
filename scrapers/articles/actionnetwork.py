5f5787b09d023d1de4d8b681898b10214bf2bac5
Braden <braden.whaley7@gmail.com>
2024-08-21 10:21:15 -0400

    the scraper will now read the article text and find teams it references for better indexing

--- scrapers/articles/actionnetwork.py
+++ scrapers/articles/actionnetwork.py
@@ -1,49 +1,44 @@
 import requests
 from lxml import html
 from datetime import datetime
+from teamExtractor import TeamAbbreviationExtractor
 
-def scrape_page_articles():
+def scrape_action_articles():
     url = "https://www.actionnetwork.com/nfl/archive/1"
     article_xpath = '/html/body/div/div/main/div/div[1]/div/div[2]/div/div[1]/div/a[3]'
     date_xpath = '/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/section[1]/div[4]/div[2]/div[2]/div[1]/div[2]'
     
-    # Adding a User-Agent header to mimic a request from a browser
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
     }
 
     try:
-        # Send a GET request to the URL with headers
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+        response.raise_for_status()
 
-        # Parse the HTML content
         tree = html.fromstring(response.content)
-
-        # Extract articles using the provided XPath
         articles = tree.xpath(article_xpath)
-
-        # Print the number of articles found
         print(f"Found {len(articles)} articles.")
 
         article_data_list = []
 
-        # Loop through each article element and extract details
         for index, article in enumerate(articles):
-            # Extract the text content of the article element
             article_text = article.text_content().strip()
             href = article.get('href')
             if href:
-                #print(f"Article {index + 1}: {article_text}\n URL: {href}")
                 article_response = requests.get(href, headers=headers)
                 article_response.raise_for_status()
 
+                # Parse the article content to create a new tree for each article
                 article_tree = html.fromstring(article_response.content)
+
+                # Use TeamAbbreviationExtractor with the appropriate div's XPath
+                extractor = TeamAbbreviationExtractor(article_response.content, '//div[contains(@class, "contentBody__main")]')
+                team_abbreviations = extractor.extract_team_abbreviations()
 
                 date_element = article_tree.xpath(date_xpath)
                 if date_element:
                     date_text = date_element[0].text_content().strip()
-
                     try:
                         date_text_clean = date_text.rsplit(' ', 1)[0]
                         date_time_obj = datetime.strptime(date_text_clean, "%b %d, %Y, %I:%M %p")
@@ -60,7 +55,8 @@ def scrape_page_articles():
                 'url': href,
                 'date': date_iso,
                 'site': "Actionnetwork.com",
-                'scraped_at': datetime.now().isoformat()
+                'scraped_at': datetime.now().isoformat(),
+                'matchup': team_abbreviations
             }
 
             article_data_list.append(article_data)
@@ -71,7 +67,3 @@ def scrape_page_articles():
         print(f"Request failed: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
-
-def scrape_action_articles():
-    articles = scrape_page_articles()
-    return articles
